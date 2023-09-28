a232564b92e35f85305330a334184986068d24a7
farinaiqbal0316 <146252889+farinaiqbal0316@users.noreply.github.com>
2023-09-28 22:01:48 +0500

    Add files via upload

---
+++ file3.php
@@ -0,0 +1,19 @@
+<?php
+$a="25";
+$b="5";
+echo "<h4>a=25<br></h4>";
+echo "<h4>b=5<br></h4>";
+$sum = $a+$b;
+echo"sum of a and b is;";
+echo"$sum<br>";
+echo "substraction of a and b is:";
+$sub=$a-$b;
+echo"$sub<br>";
+echo "multiply of a and b is:";
+$multiply=$a*$b;
+echo"$multiply<br>";
+echo "division of a and b is:";
+$division=$a/$b;
+echo"$division<br>";
+
+?>
